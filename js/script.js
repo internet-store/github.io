a1da5d7129127635ed7cf0bb037ae7bab8a77e83
internet-store <65911977+internet-store@users.noreply.github.com>
2020-05-25 19:09:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+//Скрипт под бургер-меню.
+
+$(document).ready(function() {
+	$('.header__burger').click(function(event) {
+		$('.header__burger,.header__menu').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+});
+
+
+
+// function ibg(){
+
+// let ibg=document.querySelectorAll(".ibg");
+// for (var i = 0; i < ibg.length; i++) {
+// if(ibg[i].querySelector('img')){
+// ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+// }
+// }
+// }
+
+// ibg();
